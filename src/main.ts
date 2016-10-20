bac009eecf84da2c96f828883a7b6dcb5a4f5d63
zhanglindeng <zhanglindeng@outlook.com>
2016-10-20 15:18:23 +0800

    加载bootstrap css和src/css目录下的css文件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import {AppComponent} from './app/app.component'
 
+import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 let appComponent = new AppComponent();
 appComponent.log('webpack');
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 var DefinePlugin = require('webpack/lib/DefinePlugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -18,7 +19,10 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.ts/, loader: "ts"}
+            {test: /\.ts/, loader: "ts"},
+            {test: /\.css$/, include: path.resolve('src/css'), loader: 'style!css'},
+            {test: /\.css$/, exclude: path.resolve('src'), loader: ExtractTextPlugin.extract("style", "css")},
+            {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file?name=fonts/[name].[ext]'}
         ]
     },
     resolve: {
@@ -29,6 +33,7 @@ module.exports = {
         new CommonsChunkPlugin({name: 'polyfills'}),
         new DefinePlugin({
             PRODUCTION: JSON.stringify(process.env.APP_ENVIRONMENT === 'production')
-        })
+        }),
+        new ExtractTextPlugin('[name].[hash].css')
     ]
 };
