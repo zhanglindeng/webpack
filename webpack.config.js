b4beef8456ba701f2d2c9003754c1fd4080a864c
zhanglindeng <zhanglindeng@outlook.com>
2016-10-21 07:15:26 +0800

    修复图标字体文件错误问题

--- webpack.config.js
+++ webpack.config.js
@@ -41,10 +41,9 @@ module.exports = {
                 exclude: path.resolve('src'),
                 loader: ExtractTextPlugin.extract({fallbackLoader: 'style', loader: 'css'})
             },
-            {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file?name=fonts/[name].[ext]'},
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url?limit=10000&name=fonts/[name].[ext]"
+                loader: "url?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]"
             },
             {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file?name=fonts/[name].[ext]"}
         ]
